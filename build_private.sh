26bf3e0fcfc5f729beb941e9792214d878a24b9a
Joshua A. Anderson <joaander@umich.edu>
2019-09-04 14:35:05 -0400

    Update private build script

--- build_private.sh
+++ build_private.sh
@@ -5,33 +5,36 @@ set -e
 
 DATE_TAG=$(date +%Y-%m)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTPUT=`mktemp -d -t singularity.XXXXXXX`
+umask 022
 
 cp $DIR/test/*.py $DIR/docker/nompi
 cp $DIR/*.tar.gz $DIR/docker/nompi
 docker build $DIR/docker/nompi \
-             -t software/vislab
-docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${DIR}:/output singularityware/docker2singularity:v2.6 --name software-private-vislab-${DATE_TAG} software/vislab
+             -t glotzerlab-private/software:nompi
 
 cp $DIR/test/*.py $DIR/docker/comet
 cp $DIR/*.tar.gz $DIR/docker/comet
 docker build $DIR/docker/comet \
-             -t software/comet
-docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${DIR}:/output singularityware/docker2singularity:v2.6 --name software-private-comet-${DATE_TAG} software/comet
+             -t glotzerlab-private/software:comet
 
 cp $DIR/test/*.py $DIR/docker/flux
 cp $DIR/*.tar.gz $DIR/docker/flux
 docker build $DIR/docker/flux \
-             -t software/flux
-docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${DIR}:/output singularityware/docker2singularity:v2.6 --name software-private-flux-${DATE_TAG} software/flux
+             -t glotzerlab-private/software:flux
 
 cp $DIR/test/*.py $DIR/docker/bridges
 cp $DIR/*.tar.gz $DIR/docker/bridges
 docker build $DIR/docker/bridges \
-             -t software/bridges
-docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${DIR}:/output singularityware/docker2singularity:v2.6 --name software-private-bridges-${DATE_TAG} software/bridges
+             -t glotzerlab-private/software:bridges
 
 cp $DIR/test/*.py $DIR/docker/stampede2
 cp $DIR/*.tar.gz $DIR/docker/stampede2
 docker build $DIR/docker/stampede2 \
-             -t software/stampede2
-docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${DIR}:/output singularityware/docker2singularity:v2.6 --name software-private-stampede2-${DATE_TAG} software/stampede2
+             -t glotzerlab-private/software:stampede2
+
+for label in nompi flux comet bridges stampede2
+do
+    docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${OUTPUT}:/output singularityware/docker2singularity:v2.6 --name software-${label} glotzerlab-private/software:${label}
+    mv ${OUTPUT}/*.simg /nfs/turbo/glotzer/containers/glotzerlab-private
+done
