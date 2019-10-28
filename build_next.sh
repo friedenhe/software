0ff0dc8b301ba5fc946177834d9a307074251681
Joshua A. Anderson <joaander@umich.edu>
2019-10-28 09:10:30 -0400

    Update build script

--- build_next.sh
+++ build_next.sh
@@ -7,6 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT=`mktemp -d -t singularity.XXXXXXX`
 umask 022
 
+python make_dockerfiles.py
+
 cp $DIR/test/*.py $DIR/docker/nompi
 cp $DIR/*.tar.gz $DIR/docker/nompi
 docker build $DIR/docker/nompi \
@@ -22,11 +24,6 @@ cp $DIR/*.tar.gz $DIR/docker/comet
 docker build $DIR/docker/comet \
              -t glotzerlab-next/software:comet
 
-cp $DIR/test/*.py $DIR/docker/flux
-cp $DIR/*.tar.gz $DIR/docker/flux
-docker build $DIR/docker/flux \
-             -t glotzerlab-next/software:flux
-
 cp $DIR/test/*.py $DIR/docker/bridges
 cp $DIR/*.tar.gz $DIR/docker/bridges
 docker build $DIR/docker/bridges \
@@ -37,7 +34,7 @@ cp $DIR/*.tar.gz $DIR/docker/stampede2
 docker build $DIR/docker/stampede2 \
              -t glotzerlab-next/software:stampede2
 
-for label in nompi greatlakes flux comet bridges stampede2
+for label in nompi greatlakes comet bridges stampede2
 do
     docker run -t --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock -v ${OUTPUT}:/output singularityware/docker2singularity:v2.6 --name software-${label} glotzerlab-next/software:${label}
     mv ${OUTPUT}/*.simg /nfs/turbo/glotzer/containers/glotzerlab-next
